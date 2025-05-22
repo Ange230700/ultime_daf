2d2fbe9d54bc02b156d4164307cd6f1fca30e3f5
Ange230700 <kouakouangeericstephane@gmail.com>
2025-05-22 13:45:49 +0200

    test: 💍  implement menu tab conditional rendering

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,24 @@
 // src\components\Header.tsx
 
 import { useFilter } from "../contexts/FilterContext";
+import { useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 import FiltersNav from "./FiltersNav";
 
 function Header() {
   const { title, setTitle } = useFilter();
+  const location = useLocation();
+
+  const isDetailsPage = location.pathname.startsWith("/details");
 
   return (
     <div style={{ backgroundColor: "var(--surface-card)" }}>
       <Navbar title={title} onTitleChange={setTitle} />
-      <div className="flex justify-center">
-        <FiltersNav />
-      </div>
+      {!isDetailsPage && (
+        <div className="flex justify-center">
+          <FiltersNav />
+        </div>
+      )}
     </div>
   );
 }
